d456c09cf53c5f80e8754515310b25f73d55abd4
tirth <tirth.1657@gmail.com>
2024-08-08 22:27:35 +0530

    Add SeasonalProduct & BulkProduct with override get_price method using product inheritance.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 
@@ -17,4 +19,24 @@ class Product(models.Model):
         abstract = True
 
 
+class SeasonalProduct(Product):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    discount_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
 
+    def get_price(self):
+        today = datetime.now().date()
+        if self.start_date <= today <= self.end_date:
+            return self.price * (1 - self.discount_percentage / 100)
+        return self.price
+
+
+class BulkProduct(Product):
+    bulk_quantity = models.PositiveIntegerField()
+    bulk_discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def get_price(self, quantity=1):
+        if quantity >= self.bulk_quantity:
+            return self.price * (1 - self.bulk_discount_percentage / 100)
+        return self.price
