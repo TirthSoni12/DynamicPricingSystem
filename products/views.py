8b84fa2382da6106731327a8aed2ae9f19e1a128
tirth <tirth.1657@gmail.com>
2024-08-08 22:53:22 +0530

    create ProductListCreateView & OrderCreateView views and set urls for that.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product, Order
+from .serializers import ProductSerializer, OrderSerializer
 
-# Create your views here.
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderCreateView(generics.CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
