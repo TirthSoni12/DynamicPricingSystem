44d73947455a44278719eea5db799ba03667afd9
tirth <tirth.1657@gmail.com>
2024-08-08 22:33:38 +0530

    Implement Discount, PercentageDiscount, and FixedAmountDiscount models with apply_discount method.

--- products/models.py
+++ products/models.py
@@ -40,3 +40,28 @@ class BulkProduct(Product):
         if quantity >= self.bulk_quantity:
             return self.price * (1 - self.bulk_discount_percentage / 100)
         return self.price
+
+
+class Discount(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def apply_discount(self, price):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    class Meta:
+        abstract = True
+
+
+class PercentageDiscount(Discount):
+    percentage = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def apply_discount(self, price):
+        return price * (1 - self.percentage / 100)
+
+
+class FixedAmountDiscount(Discount):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def apply_discount(self, price):
+        return max(0, price - self.amount)
