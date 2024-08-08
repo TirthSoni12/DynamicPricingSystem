d3b8a8a53b2298a1701cc9c860cd8f2e2742e687
tirth <tirth.1657@gmail.com>
2024-08-08 22:20:05 +0530

    created Product model class.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+
+    def get_price(self):
+        return self.price
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+
