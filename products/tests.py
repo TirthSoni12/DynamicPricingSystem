6cd28520b36836bc424a8d0c9fc50cfcb272aab6
tirth <tirth.1657@gmail.com>
2024-08-08 23:14:32 +0530

    implemented test cases for product.

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class ProductTests(TestCase):
+
+    def setUp(self):
+        self.product = Product.objects.create(
+            name="Test Product",
+            description="This is a test product.",
+            price=100.00
+        )
+
+    def test_product_str(self):
+        self.assertEqual(str(self.product), "Test Product")
+
+    def test_get_price(self):
+        self.assertEqual(self.product.get_price(), 100.00)
